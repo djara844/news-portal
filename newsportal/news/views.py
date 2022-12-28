d23867b4adb6a06b99f4e99dcbb63af8009968d8
Diego Jaramillo <djaramillo@vibra.fm>
2022-12-28 00:05:48 -0500

    Add view home

--- newsportal/news/views.py
+++ newsportal/news/views.py
@@ -5,4 +5,6 @@ from news.models import Author, News, Category
 # Create your views here.
 def home(request):
     authors = Author.objects.all()
-    return render(request, "index.html", {"authors": authors})
+    categories = Category.objects.all()
+    last_news = News.objects.filter().order_by('-id')[:3]
+    return render(request, "index.html", {"authors": authors, "categories": categories, "last_news": last_news})
